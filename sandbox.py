d0eb7617066aa47a9254c3843a099e137fe04644
[Sean Dillon] <[seandillon48@gmail.com]>
2024-06-14 10:11:11 -0500

    able to take weighted average with all dusts and dust shapes, still an error in ipynb

--- sandbox.py
+++ sandbox.py
@@ -19,27 +19,49 @@ import os
 
 # terp = np.interp(lam_dust1, lam_dust2, cabs_dust2)
 
-'''
 
-fig, ax = plt.subplots()
-
-ax.plot(lam_dust1, cabs_dust1, label='cabs1')
-ax.plot(lam_dust2, cabs_dust2, label='cabs2')
-ax.legend()
-plt.show()
-
-'''
 
 
 
 dustlist = [('grph1-dl.nk', 'spheres'), ('sil-dlee.nk', 'CDE'), ('FeO.nk', 'CDE2')]
 namelist = [dustlist[j][0][:-3]+dustlist[j][1]+'.dat' for j in range(len(dustlist))]
+weightlist = [1.0, 200.0, 10.0]
+# lam_max, cabs_max, csca_max = np.loadtxt(max(namelist, key=os.path.getsize), unpack=True)
+lam_final = np.geomspace(0.2, 500, num=500)
+# terp = np.interp(lam_final, lam_max, cabs_max)
 
-lam_max, cabs_max, csca_max = np.loadtxt(max(namelist, key=os.path.getsize), unpack=True)
-
-total_array = np.ndarray((3,len(lam_max),len(dustlist)))
-total_array[:,:,0] = lam_max
-total_array[0,:,1] = cabs_max
-total_array[0,:,2] = csca_max
+total_array = np.ndarray((3,len(lam_final),len(dustlist)))
+total_array[:,:,0] = lam_final
+# total_array[0,:,1] = cabs_max
+# total_array[0,:,2] = csca_max
 # for h in range(len(namelist)):
-    
+
+
+# for k in range(len(namelist)):
+#     lam, cabs, csca = np.loadtxt(namelist[k], unpack=True)
+#     total_array[k,:,1] = np.interp(lam_final, lam, cabs)
+#     total_array[k,:,2] = np.interp(lam_final, lam, csca)
+
+
+avg_array = np.ndarray((len(lam_final),3))
+avg_array[:,0] = lam_final
+for j in range(len(lam_final)):
+    avg_array[j,1] = np.average(total_array[:,j,1], weights=weightlist)
+    avg_array[j,2] = np.average(total_array[:,j,2], weights=weightlist)
+
+titlestring=''
+for g in range(len(namelist)):
+    titlestring += namelist[g][:3] + str(weightlist[g]).replace('.','')
+
+
+'''
+fig, ax = plt.subplots()
+
+ax.scatter(lam, cabs, marker='o', label='original')
+# ax.scatter(x, terp, marker='o',label='interpolated')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.legend()
+plt.show()
+
+'''
